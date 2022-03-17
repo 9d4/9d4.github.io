751e61906c7663c3ac862f5646c770bc6058bb55
Dimas Ananda <traperwaze@gmail.com>
2022-03-17 10:12:06 +0700

    remove test writing and add folder to put images
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- src/data/writings.js
+++ src/data/writings.js
@@ -1,13 +1,13 @@
 /**
  * Listing of writings contents.
  * Key is the slug, means key and slug should be same.
+ * example:
+ * test: {
+ *     title: "Test Test <h>1</h1>",
+ *     date: "2022-03-13 08:09",
+ *     content: "/contents/test.md",
+ *     slug: "test",
+ * }
  */
 
-export const writings = {
-    test: {
-        title: "Test Test <h>1</h1>",
-        date: "2022-03-13 08:09",
-        content: "/contents/test.md",
-        slug: "test",
-    }
-}
+export const writings = {}
